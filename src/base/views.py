709f786bd2b3347dbef1aa81b08b49c9b9f6f571
Egbert <eastuger@gmail.com>
2017-04-17 22:44:01 +0200

    added main page route to django

---
+++ src/base/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import View
+
+class IndexView(View):
+    """Render main page"""
+    def get(self, request):
+        """Return HTML for main app"""
+
+        abspath = open(os.path.join(settings.BASE_DIR, 'static/index.html'), 'r')
+        return HttpResponse(content=abspath.read())
